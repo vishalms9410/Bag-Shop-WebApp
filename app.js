01f0e795e829da5fa16ea5026bfbdd15034e3845
vishalms9410 <vishalms9410@gmail.com>
2026-02-12 21:22:36 +0530

    Refactor code structure for improved readability and maintainability

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const expressSession = require("express-session");
+const flash = require("connect-flash");
+
+require("dotenv").config();
+
+const indexRouter = require("./routes/index");
+const ownersRouter = require("./routes/ownersRouter");
+const usersRouter = require("./routes/usersRouter");
+const productsRouter = require("./routes/productsRouter");
+
+const db = require("./config/mongoose-connection");
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(
+    expressSession({
+        resave: false,
+        saveUninitialized: false,
+        secret: process.env.EXPRESS_SESSION_SECRET || "secret",
+    })
+);
+app.use(flash());
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+
+app.use("/", indexRouter);
+app.use("/owners", ownersRouter);
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
+
+app.listen(3000);
